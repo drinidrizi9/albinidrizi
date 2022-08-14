1ba988bc3a9800fca659c8979df40b6462952217
drinirizi9 <webdrinidrizi@gmail.com>
2022-08-14 13:58:50 +0200

    dwada

--- js/main.js
+++ js/main.js
@@ -50,19 +50,34 @@ btn.addEventListener('click', () => {
 
 
 
-Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
-    get: function () {
-        return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
-}});
 
-$('body').on('click touchstart', function () {
-    const videoElement = document.getElementById('home_video');
-    if (videoElement.playing) {
-        // video is already playing so do nothing
+
+
+
+body.addEventListener('click touchstart', () => {
+    let loadingscreen = document.querySelector('.loading-screen');
+    if (loadingscreen.playing){
+
     }
     else {
-        // video is not playing
-        // so play video now
-        videoElement.play();
+        loadingscreen.play()
     }
-});
+})
+
+
+// Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+//     get: function () {
+//         return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+// }});
+
+// .on('click touchstart', function () {
+//     const videoElement = document.getElementById('home_video');
+//     if (videoElement.playing) {
+//         // video is already playing so do nothing
+//     }
+//     else {
+//         // video is not playing
+//         // so play video now
+//         videoElement.play();
+//     }
+// });
