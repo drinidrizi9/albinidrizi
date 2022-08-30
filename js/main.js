ad5d7f41e516a336e8f9ff161565c788357986ea
drinirizi9 <webdrinidrizi@gmail.com>
2022-08-30 21:06:10 +0200

    .

--- js/main.js
+++ js/main.js
@@ -31,8 +31,12 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 
+const modal = document.getElementById('modal')
+let modalHeaderText = document.getElementById('modal-header-text')
 
 btn.addEventListener('click', () => {
+
+
     first.style.transition = '0.5s';
     second.style.transition = '0.5s';
     third.style.transition = '0.5s';
@@ -46,4 +50,46 @@ btn.addEventListener('click', () => {
     setTimeout(() => {
         btn.style.opacity = 0;
     }, 400);
+    setTimeout(() => {
+        modal.style.opacity = 1;
+    }, 1000);
+    setTimeout(() => {
+        modalHeaderText.style.opacity = 1;
+        modalHeaderText.style.transform = 'translateX(0px)'
+    }, 1500);
+    
+
+})
+
+const close = document.getElementById('close');
+close.addEventListener('click', () => {
+
+    modalHeaderText.style.opacity = 0;
+    modalHeaderText.style.transform = 'translateX(-50px)'
+    setTimeout(() => {
+        modal.style.opacity = 0;
+    }, 500);
+    setTimeout(() => {
+        btn.style.opacity = 1;
+    }, 700);
+    setTimeout(() => {
+        third.style.opacity = 1;
+    }, 900);
+    setTimeout(() => {
+        second.style.opacity = 1;
+    }, 1200);
+    setTimeout(() => {
+        first.style.opacity = 1;
+    }, 1400);
+})
+const gmailText = document.querySelector(".gmail");
+
+gmailText.addEventListener('click', async () => {
+    const content = document.getElementById('gmail').textContent;
+    await navigator.clipboard.writeText(content)
+
+    gmailText.style.color = 'var(--primary-color)'
+    setTimeout(() => {
+        gmailText.style.color = '#fff'
+    }, 1000);
 })
